6316396a9fb9d5abc05d5775d8954fd6bf35dd51
kajihara09 <m1m22a09@st.oit.ac.jp>
2023-07-08 14:36:21 +0900

    Caluculateクラスに合計，平均メソッド追加，Appクラスに出力の追加

--- app/src/main/java/sequality/App.java
+++ app/src/main/java/sequality/App.java
@@ -3,12 +3,35 @@
  */
 package sequality;
 
+import javax.swing.plaf.ProgressBarUI;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
+    public String printSumAndAve(int x, int y) {
+        Calculate calc = new Calculate();
+        return "Sum of " + x + " and " + y + " is " + calc.sum(x, y) + ".Average is " + calc.ave(x, y) + ".";
+    }
+
+    public String printSumInRangeAndAve(int x, int y) {
+        Calculate calc = new Calculate();
+        return "Sum of " + x + " to " + y + " is " + calc.sumInRange(x, y) + ".Average is " + calc.aveInRange(x, y)
+                + ".";
+    }
+
+    public String printSumOfOddAndEven(int x, int y) {
+        Calculate calc = new Calculate();
+        return "Sum of odd of " + x + " to " + y + " is " + calc.sumOfOddInRange(x, y) + ".Sum of even is "
+                + calc.sumOfEvenInRange(x, y) + ".";
+    }
+
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        App app = new App();
+        System.out.println(app.getGreeting());
+        System.out.println(app.printSumAndAve(2, 3));
+        System.out.println(app.printSumInRangeAndAve(1, 10));
+        System.out.println(app.printSumOfOddAndEven(1, 10));
     }
 }
